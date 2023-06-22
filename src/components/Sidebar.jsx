2d9f5ee0aa6f9d51e2496ff8ea655b51910a9f80
Charaf-B <Charafeddine.Belyazid@emsi-edu.ma>
2023-06-22 17:09:34 +0100

    icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,27 @@
-import { List, ListItem } from "@chakra-ui/react";
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { List, ListIcon, ListItem } from "@chakra-ui/react";
+import { color } from "framer-motion";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <List color='white' fontSize='1.2em'>
+    <List color='white' fontSize='1.2em' spacing={4}>
         <ListItem>
             <NavLink to="/">
+                <ListIcon as={CalendarIcon} color="white"/>
                 Dashboard
             </NavLink>
         </ListItem>
 
         <ListItem>
+        <ListIcon as={EditIcon} color="white"/>
             <NavLink to="/create">
                 New Task
             </NavLink>
         </ListItem>
 
         <ListItem>
+        <ListIcon as={AtSignIcon} color="white"/>
             <NavLink to="/profile">
                 Profile
             </NavLink>
